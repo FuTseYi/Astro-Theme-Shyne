21a481fc087610ebe9d31ade402e3338fa32ab3a
FuTseYi <futseyi@gmail.com>
2026-02-03 20:57:31 +0800

    update

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export type Site = {
+  title: string
+  description: string
+  href: string
+  author: string
+  locale: string
+  featuredPostCount: number
+  featuredProjectCount: number
+  postsPerPage: number
+  projectsPerPage: number
+  defaultPostBanner: string
+  defaultProjectBanner: string
+}
+
+export type SocialLink = {
+  href: string
+  label: string
+  icon?: string
+  hideBelowPx?: number
+}
+
+export type IconMap = {
+  [key: string]: string | { icon: string; color: string }
+}
